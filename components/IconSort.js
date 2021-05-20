adb629bb068124ed2f69c782faf6deb30f2bd13a
William <william.helmrath@live.com>
2021-05-19 20:55:21 -0400

    delete unnecessary file

--- components/IconSort.js
+++
@@ -1,8 +0,0 @@
-import WbSunnyIcon from '@material-ui/icons/WbSunny';
-import CloudIcon from '@material-ui/icons/Cloud';
-import Grain from '@material-ui/icons/Grain';
-
-let MatchIcon = (type)=> {
-
-
-}
